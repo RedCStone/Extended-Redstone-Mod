7c081f8d00c6cfdb31e427ca861e3d57a0e73af1
RedCStone <83328689+RedCStone@users.noreply.github.com>
2021-05-30 16:23:42 +0200

    Added half tick repeater block

--- src/main/java/redcstone/extended_redstone/HalfTickRepeaterBlock.java
+++ src/main/java/redcstone/extended_redstone/HalfTickRepeaterBlock.java
@@ -26,6 +26,6 @@ public class HalfTickRepeaterBlock extends RepeaterBlock {
 
     @Override
     protected int getUpdateDelayInternal(BlockState state) {
-        return (Integer)state.get(DELAY) * 2 - 1;
+        return (Integer)state.get(DELAY);
     }
 }
